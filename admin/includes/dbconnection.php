6ea36fa3ff9e678697a92e028f76fca9e0342120
Purna Sai Madala <purna_sai@srmap.edu.in>
2021-04-30 23:13:34 +0530

    Update dbconnection.php

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,7 +1,7 @@
 <?php 
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','purna');
+define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','hmmsdb');
 // Establish database connection.
